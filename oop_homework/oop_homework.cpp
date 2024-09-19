e09e0e075afba7e54c2da1a25f2265e2523664d0
Valerii <berezovskijvalera812@gmail.com>
2024-09-19 15:11:20 +0200

    homework 9.19.24 { Fraction }

--- oop_homework/oop_homework.cpp
+++ oop_homework/oop_homework.cpp
@@ -1,80 +1,34 @@
 #include <iostream> 
 using namespace std;
 
-void cnt(int* arr, int size, int& pos, int& neg, int& null) {
-    for (int i = 0; i < size; ++i)
-    {
-        if (arr[i] > 0) { ++pos; }
-        else if (arr[i] < 0) { ++neg; }
-        else if (arr[i] == 0) { ++null; }
+class Fraction {
+private:
+    int numerator = 0;
+    int denominator = 1;
+public:
+    void Input(int x, unsigned y) {
+        numerator = x;
+        denominator = y;
     }
-}
 
-void positive_array_init(int* arr, int size, int* from, int size_from) {
-    for (int i = 0, j = 0; i < size_from; i++) {
-        if (from[i] > 0) {
-            arr[j] = from[i];
-            j++;
-        }
+    void setRand() {
+        numerator = rand() % 100;
+        denominator = rand() % 100 + 1;
     }
-}
 
-void negative_array_init(int* arr, int size, int* from, int size_from) {
-    for (int i = 0, j = 0; i < size_from; i++) {
-        if (from[i] < 0) {
-            arr[j] = from[i];
-            j++;
-        }
+    void print() {
+        cout << numerator << '/' << denominator << endl;
     }
-}
-
-void null_array_init(int* arr, int size, int* from, int size_from) {
-    for (int i = 0, j = 0; i < size_from; i++) {
-        if (from[i] == 0) {
-            arr[j] = from[i];
-            j++;
-        }
-    }
-}
+};
 
 int main() {
     srand(time(NULL));
-    int s;
-    cin >> s;
-    int* arr = new int[s];
 
-    for (int i = 0; i < s; ++i) {
-        arr[i] = rand() % 40 - 20 + 1;
-        cout << arr[i] << ' ';
-    }
+    Fraction frac;
+    frac.setRand();
+    frac.print();
 
-    int posit_cnt = 0, neg_cnt = 0, null_cnt = 0;
-    cnt(arr, s, posit_cnt, neg_cnt, null_cnt);
-
-    cout << endl << posit_cnt << " " << neg_cnt << " " << null_cnt << endl;
-    int* positive = new int[posit_cnt];
-    int* negative = new int[neg_cnt];
-    int* null = new int[null_cnt];
-    positive_array_init(positive, posit_cnt, arr, s);
-    negative_array_init(negative, neg_cnt, arr, s);
-    null_array_init(null, null_cnt, arr, s);
-
-    for (int i = 0; i < posit_cnt; ++i) {
-        cout << positive[i] << ' ';
-    }
-    cout << endl;
-    for (int i = 0; i < neg_cnt; ++i) {
-        cout << negative[i] << ' ';
-    }
-    cout << endl;
-    for (int i = 0; i < null_cnt; ++i) {
-        cout << null[i] << ' ';
-    }
-    cout << endl;
-
-
-    delete[] positive;
-    delete[] negative;
-    delete[] null;
-    delete[] arr;
+    Fraction frac2;
+    frac2.Input(2, 3);
+    frac2.print();
 }
